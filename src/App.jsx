01f3895be429966cd7f8807ac95e74998e0d1eb3
Asilbek217 <yoldashaliyev0303@gmail.com>
2023-12-05 22:46:51 +0500

    !
    !

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import img from "./assets/logo.png";
+import { MdOutlineFacebook } from "react-icons/md";
+import { BsInstagram } from "react-icons/bs";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+import img1 from "./assets/man.png";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <nav>
+          <div className="logo">
+            <img src={img} alt="" />
+            <div className="title-logo">
+              <h4>Portfolio</h4>
+              <h6>Design</h6>
+            </div>
+          </div>
+          <div className="sections">
+            <ul>
+              <li>
+                <a href="#Home">Home</a>
+              </li>
+              <li>
+                <a href="#About">Abaut me</a>
+              </li>
+              <li>
+                <a href="#Services">Services</a>
+              </li>
+              <li>
+                <a href="#Portfolio">Portfolio</a>
+              </li>
+              <li>
+                <a href="#Blog">Personal Blog</a>
+              </li>
+              <li>
+                <a href="#Contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+          <div className="btn-hire">
+            <button type="button">Hire Me</button>
+          </div>
+        </nav>
+        <main>
+          <div className="title">
+            <div className="me">
+              <div className="hello">
+                <p>👋 Hi,I'm</p>
+                <p className="name">Asilbek</p>
+              </div>
+              <div>
+                <h1 className="designer">Product Designer</h1>
+              </div>
+            </div>
+            <div className="info">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Similique a quos non eos praesentium maiores, aspernatur, officiis
+              iste blanditiis ut omnis! Iste sapiente et esse modi perferendis
+              iure pariatur sint ratione eum?Lorem, ipsum dolor sit amet
+              consectetur adipisicing elit. Reiciendis, ipsum.
+            </div>
+            <div className="social">
+              <p>Follow Me</p>
+              <div className="media">
+                <MdOutlineFacebook />
+                <BsInstagram />
+                <FaTwitter />
+                <FaLinkedinIn />
+              </div>
+            </div>
+            <div className="btn-meeting">
+              <button className="book">Book a Meeting</button>
+              <button className="download">Download CV</button>
+            </div>
+          </div>
+          <div className="img-title">
+            <img src={img1} alt="man picture" />
+          </div>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
